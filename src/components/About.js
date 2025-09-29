b72ad7ef979e72c43ceecbc9407c7b3b35a3cae5
sadiq-zubeir <sadikmohamed0722@gmail.com>
2025-09-29 11:53:14 +0300

    done

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,15 @@
+
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data"; // adjust path if your file is actually ../data/user.js
 
 function About() {
-  return <div>About</div>;
+  return (
+    <div id="about">
+      <h2>About Me</h2>
+      <p>Write a short sentence about yourself here.</p>
+      <img src={image} alt="I made this" />
+    </div>
+  );
 }
 
 export default About;
